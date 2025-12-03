0a4cd231fb413f01dee704feea4ce9f9f1f3be81
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-03 13:21:34 -0800

    use pixi

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,7 +1,7 @@
 @echo on
 @setlocal EnableDelayedExpansion
 
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
+go build -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
 go-licenses save . --save_path=license-files --ignore=github.com/pb33f/doctor --ignore=github.com/dop251/goja || goto :error
 
 :: Manually copy licenses that go-licenses could not download
